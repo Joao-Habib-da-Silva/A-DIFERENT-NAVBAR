07a6f52f7813ae1deea7538808c5a2edae37c559
Joao Habib da Silva <contajh544@gmail.com>
2025-03-12 20:03:24 -0300

    Creating a new navbar with a diferent stylle

---
+++ script.js
@@ -0,0 +1,5 @@
+var menu = window.document.getElementById("BUTTON")
+var navbar = window.document.getElementById("navbar")
+menu.addEventListener("click", () => {
+  navbar.classList.toggle("open")          
+})
